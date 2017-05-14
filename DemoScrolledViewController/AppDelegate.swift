aa9e96fd00616267cb5f42af4e57edd3a1b4ab8d
Eric Kampman <erickampman@yahoo.com>
2017-05-14 07:00:04 -0700

    Initial Commit

---
+++ DemoScrolledViewController/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DemoScrolledViewController
+//
+//  Created by Eric Kampman on 5/14/17.
+//  Copyright © 2017 Eric Kampman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
